056b513183f9936a2c3f8225ad7c7b6defbcb42e
NALLAPRANAY <107472495+NALLAPRANAY@users.noreply.github.com>
2022-11-29 00:55:05 +0530

    Update nodemcu.ino

--- bin1/nodemcu/nodemcu.ino
+++ bin1/nodemcu/nodemcu.ino
@@ -47,7 +47,7 @@ BLYNK_CONNECTED()
   Blynk.setProperty(V3, "onImageUrl",  "https://static-image.nyc3.cdn.digitaloceanspaces.com/general/fte/congratulations_pressed.png");
   Blynk.setProperty(V3, "url", "https://docs.blynk.io/en/getting-started/what-do-i-need-to-blynk/how-quickstart-device-was-made");
 }
-BLYNK_WRITE(V0)
+/*BLYNK_WRITE(V0)
 {
   // Set incoming value from pin V0 to a variable
   //int value = param.asInt();
@@ -56,16 +56,16 @@ BLYNK_WRITE(V0)
   //for(int value=0; value<=200;value++){
   //Blynk.virtualWrite(V1, value);
   //delay(500);}
-}
+}*/
 void myTimerEvent()
 {
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
-  Blynk.virtualWrite(V2,millis() / 1000);
+ // Blynk.virtualWrite(V2,millis() / 1000);
 }
 void label(){
   
-  Blynk.virtualWrite(V1,bin_lvl);
+  Blynk.virtualWrite(V1,bin_lvl); // updates the VirtualPin V1 
 }
 void check(){
   label();
@@ -74,18 +74,18 @@ void check(){
 void setup()
 {
 
-  Serial.begin(115200);
-  serialcom.begin(9600);
-  Blynk.begin(auth, ssid, password);
-  timer.setInterval(1000L, myTimerEvent);
-  WiFi.mode(WIFI_STA);
-  ThingSpeak.begin(client);
+  Serial.begin(115200);    //To begin the serial monitor
+  serialcom.begin(9600);  //To establish serial communication
+  Blynk.begin(auth, ssid, password); //establish communication with the blynk 
+  timer.setInterval(1000L, myTimerEvent);//Sets the timmer
+  WiFi.mode(WIFI_STA);//setting to the STATION mode
+  ThingSpeak.begin(client);//To establish the communication with thingspeak
 
   delay(2500);
   Serial.print("Connecting to ");
   Serial.println(ssid);
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  WiFi.begin(ssid, password);//establish connection with wifi
+  while (WiFi.status() != WL_CONNECTED) {// check wheather the wifi is connected or not
     delay(500);
     Serial.print(".");
   }
@@ -95,28 +95,28 @@ void loop()
 {
   //internet();
   //measure();
-  Blynk.run();
-  timer.run();
+  Blynk.run();//It updates all the blynk commands
+  timer.run();//Starts the timmer
   check();
-  bin_lvl=serialcom.readStringUntil('\r');
+  bin_lvl=serialcom.readStringUntil('\r');//Reads the string from the serial communication
 
   Serial.print("Garbage Level:");
   Serial.print(bin_lvl);
   Serial.println("%");
-    upload();
+    upload();//calling the upload function
   
 }
 void upload()
 {
-  x = ThingSpeak.writeField(Channel_ID, Field_number, bin_lvl, WriteAPIKey);
+  x = ThingSpeak.writeField(Channel_ID, Field_number, bin_lvl, WriteAPIKey);//uploads the bin level to the thingspeak
  
 }
 void alert(){
   if( bin_lvl>"90"){
-    Blynk.virtualWrite(V0,1);
+    Blynk.virtualWrite(V0,1);//trigger the virtul pin V0 if bin level is greater than 90
     
   }
   else{
-    Blynk.virtualWrite(V0,0);
+    Blynk.virtualWrite(V0,0);//if bin level is less than 90 then virtual pin set to 0
     }
 }
