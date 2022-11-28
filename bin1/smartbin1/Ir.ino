1f042959378e5e676f2771d310f90bd1dcf32fb0
NALLAPRANAY <107472495+NALLAPRANAY@users.noreply.github.com>
2022-11-29 01:03:32 +0530

    Update Ir.ino

--- bin1/smartbin1/Ir.ino
+++ bin1/smartbin1/Ir.ino
@@ -1,15 +1,15 @@
 void detect_ir()
 {
-val=digitalRead(sensor);
+val=digitalRead(sensor);//reading the IR sensor value
 
 if ((val==LOW)&&(dist2<=3)){
-  servo.write(0);
+  servo.write(0);//bring the servo to intial position
   servo.detach();
-  lcd_display(); 
+  lcd_display(); //calling the lcd function
   //bin1.println(10);
-  tone(buzzer, 1800); 
+  tone(buzzer, 1800); //rings the buzzer at 1800 frequency
   delay(1000); 
-  noTone(buzzer);
+  noTone(buzzer);//tops ringing buzzer
   delay(1000);
          // ...for 1 sec
   
@@ -18,7 +18,7 @@ else if (( val==LOW )&&(dist2>3)) {
 //Change distance as per your need
  servo.attach(servoPin);
   delay(1);      
- servo.write(150);
+ servo.write(150);//sets the servo to 150 degree
  lcd_display(); 
  //bin1.println(20); 
  delay(3000); 
